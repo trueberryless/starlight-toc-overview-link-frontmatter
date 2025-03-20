ed9f74268cefcfbeeb85c790c4e4ef636b50ce21
trueberryless <trueberryless+github@gmail.com>
2025-03-20 14:07:38 +0100

    clean up

--- src/schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "astro/zod";
-
-export const extendedTableOfContentSchema = z.object({
-  topic: z.string().optional(),
-});
-
-export type ExtendedTableOfContentFrontmatterSchema = z.input<typeof extendedTableOfContentSchema>;
